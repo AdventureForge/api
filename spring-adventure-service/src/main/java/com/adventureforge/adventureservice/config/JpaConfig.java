e375e7750b0986fc267f6d50bcf6baef340f317c
Morgan <morgan.lombard@protonmail.com>
2022-05-18 11:42:05 +0200

    Prepare auditor aware bean for security implementation

--- spring-adventure-service/src/main/java/com/adventureforge/adventureservice/config/JpaConfig.java
+++ spring-adventure-service/src/main/java/com/adventureforge/adventureservice/config/JpaConfig.java
@@ -14,4 +14,12 @@ public class JpaConfig {
     public AuditorAware<String> auditorAware() {
         return new AuditorAwareImpl();
     }
+
+/*    @Bean
+    AuditorAware<String> auditorAware() {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getName);
+    }*/
 }
