b8efbd564bae90e98229d15d4111bfef4a132dcd
Morgan <morgan.lombard@protonmail.com>
2022-05-20 14:01:36 +0200

    test CI

--- spring-adventure-service/src/main/java/com/adventureforge/adventureservice/mappers/CampaignMapper.java
+++ spring-adventure-service/src/main/java/com/adventureforge/adventureservice/mappers/CampaignMapper.java
@@ -1,11 +1,4 @@
 package com.adventureforge.adventureservice.mappers;
 
-import com.adventureforge.adventureservice.dto.CampaignDTO;
-import com.adventureforge.adventureservice.entities.Campaign;
-
 public interface CampaignMapper {
-
-    CampaignDTO toDTO(Campaign campaign);
-
-    Campaign toEntity(CampaignDTO campaignDTO);
 }
