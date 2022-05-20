4a71b272033cf6460aa75d5e63f9a8508dd0dfd2
Morgan <morgan.lombard@protonmail.com>
2022-05-20 15:18:51 +0200

    test complete CI workflow

--- spring-game-service/src/main/java/com/adventureforge/gameservice/controllers/AuthorController.java
+++ spring-game-service/src/main/java/com/adventureforge/gameservice/controllers/AuthorController.java
@@ -8,11 +8,20 @@ import com.adventureforge.gameservice.services.AuthorService;
 import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -21,6 +30,7 @@ import java.util.UUID;
 import static com.adventureforge.gameservice.controllers.wrappers.ResponseWrapper.wrap;
 import static com.adventureforge.gameservice.controllers.wrappers.ResponseWrapper.wrapPageToList;
 
+@Slf4j
 @Tag(name = "Authors")
 @AllArgsConstructor
 @RestController
@@ -34,6 +44,7 @@ public class AuthorController {
     @ResponseStatus(HttpStatus.OK)
     @JsonView(value = View.External.GET.class)
     public ResponseWrapper<List<AuthorDTO>> findAllPaginated(@ParameterObject Pageable pageable) {
+        log.debug("Call findAllPaginated");
         return wrapPageToList(
                 this.authorService.findAllPaginated(pageable)
                         .map(authorMapper::toDTO)
